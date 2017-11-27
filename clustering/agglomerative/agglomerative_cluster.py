fixed_centroid option removed; formula for weights updates fixed; origin cluster replaced by centroid; distance formula added

clustering/agglomerative/agglomerative_cluster.py
@@ -84,19 +84,17 @@ class AWardPBetaCluster(AgglomerativeCluster):
     """Cluster for A-Ward agglomerative clustering with p and beta parameters
     """
 
-    def __init__(self, label, data, p, beta, weights=None, fixed_centroid=False):
+    def __init__(self, label, data, p, beta, weights=None):
         """ Constructor of AWardClusterPBeta class.
         :param int label: integer unique label of this cluster
         :param numpy.array data: of data on which the cluster is defined
         :param float p: Minkowski power
         :param float beta: power of the weights
         :param numpy.array weights: weights of the features in this cluster, shape=[1,dim_cols],
-        if None the weights will be initialized with [1/dim_cols, 1/dim_cols, ..., 1/dim_cols] array,
-        :param bool fixed_centroid: do not update centroid, use it for origin cluster"""
+        if None the weights will be initialized with [1/dim_cols, 1/dim_cols, ..., 1/dim_cols] array"""
         super().__init__(label, data)
         self._p = p
         self._beta = beta
-        self._fixed_centroid = fixed_centroid
         self._is_stable = False
         if weights is None:
             self._weights = np.full(shape=(1, self._dim_cols), fill_value=1 / self._dim_cols)
@@ -105,8 +103,7 @@ class AWardPBetaCluster(AgglomerativeCluster):
         return self._is_stable
 
     def set_points_and_update(self, points_indices):
-        """Add points to the cluster and update it. The centroid will be updated if
-        fix_centroid is set to False or centroid is None. Weights will be updated anyway.
+        """Add points to the cluster and update it.
 
         :param numpy.array points_indices: list of indices of points to add. Index is based on cluster data."""
         if len(points_indices) == 0 or set(self._points_indices) == set(points_indices):
@@ -114,23 +111,29 @@ class AWardPBetaCluster(AgglomerativeCluster):
             return
         self._points_indices = points_indices
         cluster_points = self._data[self._points_indices]
-        if not self._fixed_centroid or self._centroid is None:
-            # centroid update to the component-wise Minkowski centre of all points
-            self._centroid = minkowski_center(cluster_points, self._p)
+        # centroid update to the component-wise Minkowski centre of all points
+        self._centroid = minkowski_center(cluster_points, self._p)
         # weights update (as per 7)
         D = np.sum(np.abs(cluster_points - self.centroid) ** self._p, axis=0).astype(np.float64)
-        denominator = (D * np.sum((np.float64(1.0) / D) ** (1 / (self._p - 1))))
+        denominator = ((D ** (1 / (self._p - 1))) * np.sum((np.float64(1.0) / D) ** (1 / (self._p - 1))))
         isnan = np.isnan(denominator)
         if np.any(isnan):
-            self._weights = isnan.astype(int)/np.sum(isnan)
+            self._weights = isnan.astype(int) / np.sum(isnan)
         else:
             self._weights = np.float64(1.0) / denominator
         self._is_stable = False
         assert self._weights.shape == (self._dim_cols,)
         assert np.abs(np.sum(self._weights) - 1) < 0.0001
 
-    def centroid_to_point_distance(self, point):
-        return np.sum((self._weights ** self._beta) * np.abs(point - self.centroid) ** self._p)
+    @staticmethod
+    def distance_formula(point1, point2, weights, p, beta):
+        return np.sum((weights ** beta) * np.abs(point1 - point2) ** p)
+
+    def distance(self, point1, point2=None):
+        """Distance in terms of this cluster. If point2 is None? then distance to centroid"""
+        if point2 is None:
+            point2 = self.centroid
+        return AWardPBetaCluster.distance_formula(point1, point2, self._weights, self._p, self._beta)
 
     def merge(self, cluster1, cluster2, new_label):
         raise NotImplemented("merge of AWardPBetaCluster is not implemented")
@@ -140,6 +143,7 @@ class AWardPBetaCluster(AgglomerativeCluster):
         res += "centroid: {}\n".format(self.centroid)
         res += "weights: {}\n".format(self._weights)
         return res
+
 
 class WUndefined(BaseException):
     pass

clustering/pattern_initialization/anomalous_cluster_p_beta.py
@@ -1,6 +1,7 @@
 import numpy as np
 from clustering.agglomerative.agglomerative_cluster import AWardPBetaCluster
 from itertools import count
+from clustering.common import minkowski_center
 
 
 class APInitPB:
@@ -19,17 +20,18 @@ class APInitPB:
         self._label_counter = count()
         self._dim_rows = data.shape[0]
         self._dim_cols = data.shape[1]
-        self._origin_cluster = AWardPBetaCluster(next(self._label_counter), self._data, self._p, self._beta, fixed_centroid=True)
+        self._origin = minkowski_center(self._data, self._p)
+        # self._origin_cluster = AWardPBetaCluster(next(self._label_counter), self._data, self._p, self._beta)
 
     def _furthest_point_index(self, current_idata):
+        equal_weights = np.ones(shape=(self._dim_cols,))/self._dim_cols
+
         dist_point_to_origin = np.apply_along_axis(
-            func1d=lambda point: self._origin_cluster.centroid_to_point_distance(point),
+            func1d=lambda point: AWardPBetaCluster.distance_formula(
+                point, self._origin, equal_weights, self._p, self._beta),
             axis=1,
             arr=current_idata[:, 1:])
         return current_idata[:, 0][dist_point_to_origin.argmax()]
-
-    def _distance(self, point, centroid, weights):
-        return np.sum((weights ** self._beta) * np.abs(point - centroid) ** self._p)
 
     def __call__(self):
         current_idata = self._idata
@@ -37,30 +39,21 @@ class APInitPB:
             current_index = current_idata[:, 0].astype(int)  # this is index of the points in initial data terms
             current_data = current_idata[:, 1:]  # this is current, cropped data without index
 
-            self._origin_cluster.set_points_and_update(current_index)
             tentative_centroid_index = self._furthest_point_index(current_idata)  # step 2
             anomalous_cluster = AWardPBetaCluster(next(self._label_counter), self._data, self._p, self._beta)
-            normal_points_indices = []  # for safety
             anomalous_cluster.set_points_and_update(np.array([tentative_centroid_index]))
             while not anomalous_cluster.is_stable():
                 dist_point_to_origin = np.apply_along_axis(
-                    func1d=lambda point: self._origin_cluster.centroid_to_point_distance(point),
+                    func1d=lambda point: anomalous_cluster.distance(point, self._origin),
                     axis=1, arr=current_data)
 
-                # dist_point_to_origin = np.apply_along_axis(
-                #     func1d=lambda point: self._distance(point, self._origin_cluster.centroid,
-                #                                         anomalous_cluster._weights),
-                #     axis=1, arr=current_data)
                 dist_point_to_tentative_centroid = np.apply_along_axis(
-                    func1d=lambda point: anomalous_cluster.centroid_to_point_distance(point),
+                    func1d=lambda point: anomalous_cluster.distance(point),
                     axis=1, arr=current_data)
 
-                # anomaly = dist_point_to_origin >= dist_point_to_tentative_centroid
                 anomaly = dist_point_to_origin >= dist_point_to_tentative_centroid
                 anomalous_points_indices = current_index[anomaly]
-                normal_points_indices = current_index[~anomaly]
                 anomalous_cluster.set_points_and_update(anomalous_points_indices)  # step 3 and 4,5 inside update
-                self._origin_cluster.set_points_and_update(normal_points_indices)
             self._clusters.append(anomalous_cluster)  # step 6
             current_idata = current_idata[~anomaly]
 

tests/api_p_beta/test_api_p_beta.py
@@ -1,28 +1,28 @@
 import numpy as np
 
 from clustering.pattern_initialization.anomalous_cluster_p_beta import APInitPB
+from tests.tools import transformation_exists
 
 
-DATA_DIR = "data/"
+import os
+
+this_file = __file__
+i = this_file.rfind(os.path.sep)
+DATA_DIR = this_file[:i] + "/data/"
+print(this_file)
 
 
 def test_symmetric_16points():
     data = np.loadtxt('{}symmetric_15points.pts'.format(DATA_DIR))
-    # data = np.loadtxt('{}iris.pts'.format(DATA_DIR))
-    run_api_p_beta = APInitPB(data, p=2, beta=2)
+    run_api_p_beta = APInitPB(data, p=5, beta=5)
     result = run_api_p_beta()
-    print(run_api_p_beta._clusters)
+    actual = np.loadtxt('{}symmetric_15points.lbs'.format(DATA_DIR), dtype=int)
     print(result)
-    # actual = np.loadtxt('{}symmetric_15points.lbs'.format(DATA_DIR), dtype=int)
-    # assert transformation_exists(actual, result)
+    assert transformation_exists(actual, result)
 
-#
-# def test_iris():
-#     data = np.loadtxt('{}iris.pts'.format(DATA_DIR))
-#     result = a_ward(data, 3, np.arange(0, len(data), dtype=int))
-#     # compare with sklearn implementation
-#     from sklearn.cluster import AgglomerativeClustering
-#     model = AgglomerativeClustering(n_clusters=3)
-#     model.fit(data)
-#     actual = model.labels_
-#     assert transformation_exists(actual, result)
+
+def test_iris():
+    data = np.loadtxt('{}iris.pts'.format(DATA_DIR))
+    run_api_p_beta = APInitPB(data, p=4, beta=2)
+    result = run_api_p_beta()
+    print(result)