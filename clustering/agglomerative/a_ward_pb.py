35fce67e3d7f62430bfa1e28e0c145305eb03905
Petr Eremeykin <eremeykin@gmail.com>
2018-01-23 22:22:13 +0300

    add cluster strucutre property to A Ward p beta algorithm

--- clustering/agglomerative/a_ward_pb.py
+++ clustering/agglomerative/a_ward_pb.py
@@ -6,6 +6,7 @@ class AWardPB:
         self._k_star = k_star
         self._cluster_structure = cluster_structure
         self._initial_cluster_count = cluster_structure.clusters_number
+        self._completed = False
 
     def _nearest(self, clusters):
         n_clusters = len(clusters)
@@ -21,6 +22,13 @@ class AWardPB:
         assert distance_matrix[min_row, min_col] == distance_matrix.min()
         return min_row, min_col
 
+    @property
+    def cluster_structure(self):
+        if not self._completed:
+            raise Exception("Can't return clusters because the algoritm must be "
+                            "executed first. Please, use __call__ to run algorithm.")
+        return self._cluster_structure
+
     def __call__(self):
         while self._cluster_structure.clusters_number > self._k_star:
             clusters = list(self._cluster_structure.clusters)
@@ -29,4 +37,5 @@ class AWardPB:
             c1 = clusters[i]
             c2 = clusters[j]
             self._cluster_structure.merge(c1, c2)
+        self._completed = True
         return self._cluster_structure.current_labels()
